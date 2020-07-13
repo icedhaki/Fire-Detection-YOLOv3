9bd96c0b6c7b24144259c0a97b770657a79e6957
Khaki <shyntask@lafayette.edu>
2020-07-13 16:12:20 -0400

    added streamon

--- drone.py
+++ drone.py
@@ -2,12 +2,14 @@ from djitellopy import Tello
 import cv2
 import time
 
+######################################################################
 width = 320  # WIDTH OF THE IMAGE
 height = 240  # HEIGHT OF THE IMAGE
 startCounter =0   #  0 FOR FIGHT 1 FOR TESTING
+######################################################################
 
+# CONNECT TO TELLO
 tello = Tello()
-
 tello.connect()
 
 tello.for_back_velocity = 0
@@ -17,11 +19,8 @@ tello.yaw_velocity = 0
 tello.speed = 0
 
 
-tello.streamoff()
-tello.streamon()
-
-
 while True:
+    tello.streamon()
 
     # GET THE IMGAE FROM TELLO
     frame_read = tello.get_frame_read()
@@ -31,17 +30,14 @@ while True:
     # TO GO UP IN THE BEGINNING
     if startCounter == 0:
         tello.takeoff()
-        time.sleep(8)
-        tello.rotate_clockwise(90)
-        time.sleep(3)
         tello.move_left(35)
-        time.sleep(3)
+        tello.rotate_clockwise(90)
         tello.land()
         startCounter = 1
 
     # # SEND VELOCITY VALUES TO TELLO
-    # if me.send_rc_control:
-    #     me.send_rc_control(me.left_right_velocity, me.for_back_velocity, me.up_down_velocity, me.yaw_velocity)
+    if tello.send_rc_control:
+        tello.send_rc_control(tello.left_right_velocity, tello.for_back_velocity, tello.up_down_velocity, tello.yaw_velocity)
 
     # DISPLAY IMAGE
     cv2.imshow("MyResult", img)
