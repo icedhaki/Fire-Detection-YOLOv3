c575eca13df518954019990947d2060aeea421c4
unknown <User@ALPHA309-05.ad.lafayette.edu>
2020-07-26 23:25:32 -0400

    added distance calculation algorithm

---
+++ yolo_object_detection1.py
@@ -0,0 +1,90 @@
+import cv2
+import numpy as np
+import time
+import math
+
+# Load Yolo
+net = cv2.dnn.readNet("yolov3_training_new.weights", "yolov3_testing.cfg")
+classes = ["Fire"]
+
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# Loading camera
+cap = cv2.VideoCapture(1)
+font = cv2.FONT_HERSHEY_PLAIN
+starting_time = time.time()
+frame_id = 0
+
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(4) # float
+
+right = 0
+left = 0
+print(width)
+print(height)
+
+rad = math.radians(57.885)
+coefficient = (6.8*640)/(2*math.tan(rad/2))
+print(coefficient)
+while True:
+    _, frame = cap.read()
+    frame_id += 1
+    height, width, channels = frame.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                if x > 640:
+                    right = x
+                else:
+                    left = x
+                boxes.append([x, y, w, h])
+                if x>0:
+                    print("right:" + str(right))
+                    print("left:" + str(left))
+                    distance = (6.8*640)/(2*math.tan(rad/2)*(right-640-left))
+                    print("distance:" + str(distance))
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.4, 0.3)
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            confidence = confidences[i]
+            color = colors[class_ids[i]]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + 30), color, -1)
+            cv2.putText(frame, label + " " + str(round(confidence, 2)), (x, y + 30), font, 3, (255,255,255), 3)
+
+    elapsed_time = time.time() - starting_time
+    fps = frame_id / elapsed_time
+    cv2.putText(frame, "FPS: " + str(round(fps, 2)), (10, 50), font, 3, (0, 0, 0), 3)
+    cv2.imshow("Image", frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
